f81e6d876ac7e1090ece1d343a5d439e091c541e
JongHyunLee84 <whdgus3697@naver.com>
2022-12-02 10:41:42 +0900

    first

---
+++ Example-Git/Example_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_GitApp.swift
+//  Example-Git
+//
+//  Created by 이종현 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct Example_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
